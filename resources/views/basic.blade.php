ff620d285458104fcd9efbab4d62d4878e82f6f5
Farriq muwaffaq <farriqmuwaffaq100@gmail.com>
2024-04-19 19:43:53 +0700

    first commit

---
+++ resources/views/basic.blade.php
@@ -0,0 +1,25 @@
+@extends('templates.main')
+@section('title')
+    Basic page
+@endsection
+@section('breadcrumbs')
+    <div class="col-12 col-md-6 order-md-2 order-first">
+        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Layout Default</li>
+            </ol>
+        </nav>
+    </div>
+@endsection
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi? Ullam quaerat
+            similique iusto
+            temporibus, vero aliquam praesentium, odit deserunt eaque nihil saepe hic deleniti? Placeat
+            delectus
+            quibusdam ratione ullam!
+        </div>
+    </div>
+@endsection
